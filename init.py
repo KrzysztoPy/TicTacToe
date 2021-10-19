5b86fe53eae9f1202fe4897b3eb72f5d5407828d
Kriss <geraltrivia19@gmail.com>
2021-10-19 16:14:08 +0200

    Displaying text in console for users.

---
+++ init.py
@@ -0,0 +1,7 @@
+from extension_dic.player_class import Player
+
+first_player = Player()
+second_player = Player()
+
+first_player.setting_name_the_of_players()
+second_player.setting_name_the_of_players()
