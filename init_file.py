5edf9b62d8e51f8a71a04b403a636e1183c218bb
Kriss <geraltrivia19@gmail.com>
2021-10-21 18:01:36 +0200

    Ordering text functions

--- init_file.py
+++ init_file.py
@@ -1,4 +1,5 @@
 from main_menu_dic.main_menu import *
 
-displaying_the_name_of_the_game()
-data_analysis_for_compilance_with_the_guidelines(selecting_an_action_option)
+
+selected_option = main_menu_init()
+go_to_the_selected_option(selected_option)
---
+++ main_menu_dic/main_menu_text.py
@@ -0,0 +1,20 @@
+def game_name_text():
+    return '\nWelcome in simple Tic Tac Toe \n'
+
+
+def menu_option_text():
+    menu_list = ['1. Start', '2. Statistic', '3. Exit']
+    return menu_list
+
+
+def question_about_selected_option_text():
+    return '\n Select option: '
+
+
+def error_wrong_data_type_text():
+    return '\n Incorrect data type. You can select only digt. Please try again.\n'
+
+
+def error_wrong_range_of_selected_data_text():
+    return '\n Incorrect data range. You can selected option from range {} to {}. Please try again. \n'.format(1,
+                                                                                                               menu_option_text().__len__())
