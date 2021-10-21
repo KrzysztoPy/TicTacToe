3e68f9d23cc0d15c142f48a52476ed1444f3ca09
Kriss <geraltrivia19@gmail.com>
2021-10-21 18:00:32 +0200

    Main menu and test for them excluding go_to_the_selected_option and tests for them.

--- all_tests/test_main_menu.py
+++ all_tests/test_main_menu.py
@@ -1,8 +1,17 @@
 from main_menu_dic.main_menu import *
+from main_menu_dic.main_menu_text import menu_option_text
+from unittest.mock import patch, Mock, MagicMock
 
 
 def test_displaying_the_name_of_the_game():
     displaying_the_name_of_the_game()
+
+
+def test_selecting_and_input_option_from_user(monkeypatch):
+    input_text = 'doesn\'t have matter'
+    monkeypatch.setattr('builtins.input', lambda _: input_text)
+    result = selecting_and_input_option_from_user(menu_option_text(), question_about_selected_option_text())
+    assert input_text == result
 
 
 def test_check_which_user_data_is_integer():
@@ -10,5 +19,21 @@ def test_check_which_user_data_is_integer():
     expected_result = [1, 23, None, 46, None, None]
 
     for counter in range(len(samples)):
-        result = check_which_user_data_is_integer(samples[counter])
+        data_from_user = samples[counter]
+        result = check_which_user_data_is_integer(data_from_user)
         assert result == expected_result[counter]
+
+
+def test_checking_the_correctness_of_the_range():
+    data_from_user = None
+    range_from = 0
+    range_to = len(menu_option_text())
+
+    samples = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    expected_result = [None, None, 1, 2, 3, None, None, None, None, None, None, None]
+
+    for counter in range(len(samples)):
+        data_from_user = samples[counter]
+        data_from_user = checking_the_correctness_of_the_range(range_from, range_to, data_from_user)
+
+        assert data_from_user == expected_result[counter]
--- main_menu_dic/main_menu.py
+++ main_menu_dic/main_menu.py
@@ -1,50 +1,40 @@
 from extension_dic.view_text_in_console import set_input
 from main_menu_dic.main_menu_text import *
 
-data_from_user = None
+range_from = 0
+range_to = len(menu_option_text())
 
 
 def displaying_the_name_of_the_game():
-    set_input(game_name_txt())
+    set_input(game_name_text())
 
 
-def selecting_an_action_option():
-    set_input(menu_option_txt(), '\n')
-    return input(question_about_selected_option_txt())
+def selecting_and_input_option_from_user(options_text, question_about_selected_option_text):
+    set_input(options_text, '\n')
+    return input(question_about_selected_option_text)
 
 
-def data_analysis_for_compilance_with_the_guidelines():
-    user_data = check_which_user_data_is_integer(data_from_user)
-
-    while True:
-        if user_data or user_data == 0:
-            if checking_the_correctness_of_the_range(user_input_data):
-                return user_data
-
-
-def check_which_user_data_is_integer(user_data):
+def check_which_user_data_is_integer(data_from_user):
     try:
-        user_data = int(user_data)
-        return user_data
+        data_from_user = int(data_from_user)
+        return data_from_user
     except (ValueError, TypeError):
-        set_input(error_wrong_data_type_txt())
+        set_input(error_wrong_data_type_text())
         return None
 
 
-def checking_the_correctness_of_the_range(is_integer):
-    if 0 < is_integer < 4:
-        return is_integer
+def checking_the_correctness_of_the_range(range_from, range_to, data_from_user):
+    if range_from >= data_from_user or data_from_user > range_to:
+        set_input(error_wrong_range_of_selected_data_text())
+        return None
     else:
-        set_input(error_wrong_range_of_selected_data())
-        return None
+        return data_from_user
 
 
 def go_to_the_selected_option(user_selection):
     if user_selection == 1:
+        number_of_player()
         new_gameplay = New_match()
-        # new_gameplay.
-
-        # draw_who_play_x_and_who_o()
         return 1
 
     elif user_selection == 2:
@@ -53,5 +43,16 @@ def go_to_the_selected_option(user_selection):
     elif user_selection == 3:
         exit_game()
 
-# data_from_user = selecting_an_action_option()
-# data_analysis_for_compilance_with_the_guidelines()
+
+def main_menu_init():
+    global range_from, range_to
+    data_from_user = None
+
+    while True:
+        data_from_user = check_which_user_data_is_integer(
+            selecting_and_input_option_from_user(menu_option_text(), question_about_selected_option_text()))
+
+        if data_from_user is not None:
+            data_from_user = checking_the_correctness_of_the_range(range_from, range_to, data_from_user)
+            if data_from_user is not None:
+                return data_from_user
